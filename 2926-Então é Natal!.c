17012fb64d4c85337e0f7a517f3ae994b6ba249e
Geisy <74117146+Geisianny@users.noreply.github.com>
2021-03-18 20:52:53 -0300

    Create 2926-Então é Natal!.c

---
+++ 2926-Então é Natal!.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+void conte(int num){
+    int i;
+
+    for(i =0;i<num;i++){
+        printf("a");
+    }
+}
+int main()
+{
+    int i,num;
+
+    scanf("%d",&num);
+
+    printf("Ent");
+    conte(num);
+
+    printf("o eh N");
+    conte(num);
+
+    printf("t");
+    conte(num);
+
+    printf("l!\n");
+
+    return 0;
+}
